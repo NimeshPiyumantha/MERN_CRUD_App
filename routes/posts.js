a2ca99e31b9c3331d1ae26ac77e7ddccabfc524e
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-19 16:46:34 +0530

    Add get posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { find } = require('../models/posts');
 const Posts = require('../models/posts');
 
 const router = express.Router();
@@ -15,6 +16,21 @@ router.post('/post/save', (req, res) => {
         }
         return res.status(200).json({
             success: "Posts saved Successfully"
+        });
+    });
+});
+
+//get posts
+router.get('/post/get', (req, res) => {
+    Posts.find().exec((err, posts) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            existingPosts: posts
         });
     });
 });
