f06df18d4a27dc96ba4dc9488ad9197eb1da1ab4
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-19 15:50:22 +0530

    Add db Connection conncted code

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const port = 8000;
+const DB_URL = 'mongodb+srv://root:1234@mernapp.bgfziqs.mongodb.net/mernCrud?retryWrites=true&w=majority'
+
+mongoose.connect(DB_URL).then(() => {
+        console.log('DB Conncted');
+    })
+    .catch((err) => console.log('DB connection error.', err))
 
 app.listen(port, () => {
     console.log(`App is running o ${port}`);
