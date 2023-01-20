e96b000d0fc178c81d6e2088f64710c4f1128194
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-20 21:03:37 +0530

    Add App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    componentDidMount() {
+        this.retrievePosts();
+    }
+
+    retrievePosts() {
+        axios.get("http://localhost:8000/posts").then(res => {
+            if (res.data.success) {
+                this.setState({
+                    posts: res.data.existingPosts
+                });
+
+                console.log(this.state.posts)
+            }
+
+        });
+    }
+
+    render() {
+        return ( <
+            div >
+
+            <
+            /div>
+        )
+    }
 }
-
-export default App;
