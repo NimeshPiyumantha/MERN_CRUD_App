00f0936d37a0be89088c357f8dac6ccf67714a7c
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-20 20:04:06 +0530

    import cors to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ const postRoutes = require('./routes/posts');
 
 //app middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use(postRoutes);
 
