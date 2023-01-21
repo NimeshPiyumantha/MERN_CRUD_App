c2bbf332526d7704f19ceaac430de77518ff2e21
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-21 20:18:14 +0530

    Update content

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
 export default class App extends Component {
@@ -15,24 +15,28 @@ export default class App extends Component {
     }
 
     retrievePosts() {
-        axios.get("http://localhost:8000/posts").then(res => {
+        axios.get("http://localhost:8000/post/get").then(res => {
             if (res.data.success) {
                 this.setState({
                     posts: res.data.existingPosts
                 });
-
                 console.log(this.state.posts)
             }
-
         });
     }
 
     render() {
-        return ( <
-            div >
+        return (
+            <div>
+                {this.state.posts.map(posts =>(
+                    <div>
+                        <p>{posts.name}</p>
+                        <p>{posts.state}</p>
+                        <p>{posts.salary}</p>
 
-            <
-            /div>
+                    </div>
+                ))}
+            </div>
         )
     }
 }
